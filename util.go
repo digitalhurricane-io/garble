267a1c912c0435f47f37b1e2acf2d9c7b8ee54f5
Paul Hendricks <paul@digitalhurricane.io>
2020-04-22 15:56:40 -0600

    remove dead code

--- util.go
+++ util.go
@@ -9,15 +9,3 @@ func isGoFile(path string) bool {
 	return filepath.Ext(path) == ".go"
 }
 
-// Checks whether the user actually passed the flag.
-// The flag has a value regardless of whether the user actually passed it,
-// because of the default. Thus the need for this function.
-func wasFlagPassed(flagSet *flag.FlagSet, name string) bool {
-	found := false
-	flagSet.Visit(func(f *flag.Flag) {
-		if f.Name == name {
-			found = true
-		}
-	})
-	return found
-}
